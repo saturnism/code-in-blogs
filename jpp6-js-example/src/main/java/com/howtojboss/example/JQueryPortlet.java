9b1bd0b0acd65c1c49725945f9ec395a3fb1024c
saturnism <saturnism@gmail.com>
2013-01-24 12:59:43 -0800

    example code for howtojboss.com

---
+++ jpp6-js-example/src/main/java/com/howtojboss/example/JQueryPortlet.java
@@ -0,0 +1,46 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011 Red Hat Inc. and/or its affiliates and other
+ * contributors as indicated by the @author tags. All rights reserved.
+ * See the copyright.txt in the distribution for a full listing of
+ * individual contributors.
+ * 
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.howtojboss.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.portlet.GenericPortlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import javax.portlet.UnavailableException;
+
+public class JQueryPortlet extends GenericPortlet {
+
+	/* (non-Javadoc)
+	 * @see javax.portlet.GenericPortlet#doView(javax.portlet.RenderRequest, javax.portlet.RenderResponse)
+	 */
+	@Override
+	protected void doView(RenderRequest request, RenderResponse response)
+			throws PortletException, IOException, UnavailableException {
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		writer.write("<div class='jquery-portlet'>jQuery Portlet Example for <a href='http://howtojboss.com'>HowToJBoss.com</a><br/>You should see an alert window showing you the jQuery version.</div>");
+		writer.close();
+	}
+}
